01525d7ff5291a96b0c6ad879535f4cabed870e0
Faneva-Rb <rabarivelofaneva@gmail.com>
2026-08-14 15:45:45 +0200

    add articles and comments app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [ArticlesModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
