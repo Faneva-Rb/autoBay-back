c3502d86be9fb98167ca0e80c1d8da47ac41267f
Faneva-Rb <rabarivelofaneva@gmail.com>
2026-08-15 10:33:24 +0200

    Fix bug prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,17 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }));
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Mon API')
+    .setDescription('API CRUD avec NestJS')
+    .setVersion('1.0')
+    .addTag('articles')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); 
 
   await app.listen(3000);
   console.log('🚀 API TypeORM sur http://localhost:3000/api');
