7e6038e3be383ce079834f497e247af6894bf154
Faneva-Rb <rabarivelofaneva@gmail.com>
2026-08-17 13:17:52 +0200

    Feature auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    PrismaModule, PostsModule],
+    PrismaModule,
+    AuthModule,
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from "bcrypt"
+
+@Injectable()
+export class AuthService {
+    
+}
