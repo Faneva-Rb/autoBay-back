59902ce38acceb1dda49dd9d57a8621fa58f2d4c
Faneva-Rb <rabarivelofaneva@gmail.com>
2026-09-08 15:57:26 +0200

    remove log

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -5,9 +5,7 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, any>{
     intercept(context: ExecutionContext, next: CallHandler<T>): Observable<any>{
         const response = context.switchToHttp().getResponse()
         const request = context.switchToHttp().getRequest()
-        
-        console.log(response.statusCode);
-        
+                
         
         return next.handle().pipe(
            
